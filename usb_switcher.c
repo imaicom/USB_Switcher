9437c1ecb7e16fa766ce1d7fdd46e1089f3813d7
KURURU <i@okako.com>
2019-09-13 14:29:24 +0900

    modified:   build/default/production/usb_switcher.p1 	modified:   build/default/production/usb_switcher.pre 	modified:   dist/default/production/USB_Switcher.production.cmf 	modified:   dist/default/production/USB_Switcher.production.elf 	modified:   dist/default/production/USB_Switcher.production.lst 	modified:   dist/default/production/USB_Switcher.production.map 	modified:   dist/default/production/USB_Switcher.production.obj 	modified:   dist/default/production/USB_Switcher.production.rlf 	modified:   dist/default/production/USB_Switcher.production.sdb 	modified:   dist/default/production/USB_Switcher.production.sym 	modified:   nbproject/private/private.xml modified:   usb_switcher.c

--- usb_switcher.c
+++ usb_switcher.c
@@ -12,7 +12,22 @@
 //
 // USB port cut and plug... RESET
 
+// Pin Layout
 
+// /MCLR  1    28 RB7
+// RA0    2    27 RB6
+// RA1    3    26 RB5
+// RA2    4    25 RB4
+// RA3    5    24 RB3
+// RA4    6    23 RB2
+// RA5    7    22 RB1
+// Vss    8    21 RB0
+// RA7    9    20 Vdd
+// RA6   10    19 Vss
+// RC0   11    18 RC7
+// RC1   12    17 RC6
+// RC2   13    16 RC5
+// RC3   14    15 RC4
 
 // PIC16F1938 Configuration Bit Settings
 
